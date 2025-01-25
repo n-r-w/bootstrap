4f5ea912730de6f0005035ca3a728ea26247ffb8
Roman Nikulenkov <nrw@yandex.ru>
2025-01-25 11:46:35 +0300

    separate options

--- bootstrap.go
+++ bootstrap.go
@@ -15,69 +15,6 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-// Option - function for configuring Bootstrap.
-type Option func(*Bootstrap)
-
-// WithStopTimeout - sets the timeout for graceful shutdown.
-func WithStopTimeout(timeout time.Duration) Option {
-	return func(b *Bootstrap) {
-		b.stopTimeout = timeout
-	}
-}
-
-// WithStartTimeout - sets the timeout for graceful startup.
-func WithStartTimeout(timeout time.Duration) Option {
-	return func(b *Bootstrap) {
-		b.startTimeout = timeout
-	}
-}
-
-// WithHealthCheck - sets health.Handler for service readiness check.
-func WithHealthCheck(hch IHealthChecher) Option {
-	return func(b *Bootstrap) {
-		b.hch = hch
-	}
-}
-
-// WithOrdered - sets services that must be started in a specific order.
-// Shutdown happens in reverse order after stopping services that don't require ordering.
-func WithOrdered(services ...IService) Option {
-	return func(b *Bootstrap) {
-		b.ordered = services
-	}
-}
-
-// WithUnordered - sets services that can be started in parallel.
-// Shutdown also happens in parallel before stopping services that require ordering.
-func WithUnordered(services ...IService) Option {
-	return func(b *Bootstrap) {
-		b.unordered = services
-	}
-}
-
-// WithAfterStart - sets services that should be started after all others.
-// Startup happens in order after starting all other services.
-// Shutdown happens in reverse order before stopping all other services.
-func WithAfterStart(services ...IService) Option {
-	return func(b *Bootstrap) {
-		b.afterStart = services
-	}
-}
-
-// WithRunFunc - sets a function to run. If set, the function will be called and then work will be completed.
-func WithRunFunc(runFunc func(context.Context) error) Option {
-	return func(b *Bootstrap) {
-		b.runFunc = runFunc
-	}
-}
-
-// WithLogger - sets logger for logging.
-func WithLogger(logger ILogger) Option {
-	return func(b *Bootstrap) {
-		b.logger = logger
-	}
-}
-
 // Bootstrap - helper for starting services.
 type Bootstrap struct {
 	appName    string
